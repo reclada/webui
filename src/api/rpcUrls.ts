5764cf216c6170b064dc031eb6bcb9dce475bca5
Dmitry Krupenya <dkrupenya@gmail.com>
2021-05-25 14:28:24 +0300

    fix api name

--- src/api/rpcUrls.ts
+++ src/api/rpcUrls.ts
@@ -2,7 +2,7 @@ export const rpcUrls = {
   createRecladaObject: '/api/rpc/reclada_object_create',
   getRecladaObjectList: '/api/rpc/reclada_object_list',
   getRecladaObjectsFromList: '/api/rpc/reclada_object_list_related',
-  addToList: '/api/rpc/list_add',
-  removeFromList: '/api/rpc/list_drop',
+  addToList: '/api/rpc/reclada_object_list_add',
+  removeFromList: '/api/rpc/reclada_object_list_drop',
   updateRecladaObject: '/api/rpc/reclada_object_update',
 };
