702c3c540ecac68d28ed0c5cdc2afb46a03c08be
anastasia.turygina <anastasia.turygina@quantori.com>
2021-08-02 14:28:47 +0300

    lint fix

--- src/pages/SearchResultPage/SearchResultMain/ResultToolbar/ResultToolbar.tsx
+++ src/pages/SearchResultPage/SearchResultMain/ResultToolbar/ResultToolbar.tsx
@@ -1,5 +1,5 @@
 import { DownOutlined } from '@ant-design/icons/lib';
-import {Tooltip} from 'antd';
+import { Tooltip } from 'antd';
 import React, { FC } from 'react';
 
 import { ReactComponent as Settings } from '../../../../resources/settings.svg';
@@ -19,7 +19,7 @@ export const ResultToolbar: FC<ResultToolbarProps> = function ResultToolbar({
 }) {
   return (
     <div className={classNames(className, style.root)}>
-      <Tooltip placement="bottom" title="List of actions" color={'#243B50'}>
+      <Tooltip color={'#243B50'} placement="bottom" title="List of actions">
         <div className={style.actions}>
           Action <DownOutlined />
         </div>
--- src/pages/SearchResultPage/SearchResultMain/ResultToolbar/SortSettings/SortSettings.tsx
+++ src/pages/SearchResultPage/SearchResultMain/ResultToolbar/SortSettings/SortSettings.tsx
@@ -11,8 +11,7 @@ type SortSettingsProps = {
 
 const sortTypes: SelectOption[] = [
   {
-    label: '' +
-      'Relevance',
+    label: '' + 'Relevance',
     value: 'rel',
   },
   {
