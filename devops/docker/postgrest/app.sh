3e52d897ac1cbfc61b32b7789aa9f43481fcd0d4
Gitlab runner <Gitlab@reclada.com>
2021-12-01 09:17:10 +0000

    Merge branch 'add/SAST' into 'main'
    Add/sast
    
    See merge request developers/webui!37

--- devops/docker/postgrest/app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-__DIR="$( realpath "$( dirname "${BASH_SOURCE[0]}" )" )"
-BASE_DIR="${BASE_DIR:-${__DIR}}"
-sudo -E /bin/bash "${BASE_DIR}/main.sh"
--- devops/docker/postgrest/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo 'j2 templating'
-j2 /etc/postgrest.conf.j2 > /etc/postgrest.conf
-
-echo 'START supervisord'
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
